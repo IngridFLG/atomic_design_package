995bff4c19bcd40ccd80e9d63aebcdc23e2428d6
INFlorez <ingrid.florez@pragma.com.co>
2025-02-04 19:21:56 -0500

    se agrega template

--- lib/pages/login_register_page.dart
+++ lib/pages/login_register_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import '../organisms/login_register_tabs.dart';
-import '../organisms/header.dart';
+import '../templates/login_register_template.dart';
 
 /// Página para login y registro.
 class LoginRegisterPage extends StatelessWidget {
@@ -10,27 +9,14 @@ class LoginRegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login & Registro')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            const Header(
-              title: 'Bienvenido',
-              subtitle: 'Por favor inicia sesión o regístrate para continuar',
-            ),
-            Expanded(
-              child: LoginRegisterTabs(
-                onLogin: () => print('Iniciar Sesión'),
-                onRegister: () => print('Registrarse'),
-                loginUsernameController: TextEditingController(),
-                loginPasswordController: TextEditingController(),
-                registerUsernameController: TextEditingController(),
-                registerEmailController: TextEditingController(),
-                registerPasswordController: TextEditingController(),
-              ),
-            ),
-          ],
-        ),
+      body: LoginRegisterTemplate(
+        onLogin: () => print('Iniciar Sesión'),
+        onRegister: () => print('Registrarse'),
+        loginUsernameController: TextEditingController(),
+        loginPasswordController: TextEditingController(),
+        registerUsernameController: TextEditingController(),
+        registerEmailController: TextEditingController(),
+        registerPasswordController: TextEditingController(),
       ),
     );
   }
---
+++ lib/templates/login_register_template.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import '../organisms/login_register_tabs.dart';
+import '../organisms/header.dart';
+
+/// Template para la página de Login y Registro.
+class LoginRegisterTemplate extends StatelessWidget {
+  final VoidCallback onLogin;
+  final VoidCallback onRegister;
+  final TextEditingController loginUsernameController;
+  final TextEditingController loginPasswordController;
+  final TextEditingController registerUsernameController;
+  final TextEditingController registerEmailController;
+  final TextEditingController registerPasswordController;
+
+  const LoginRegisterTemplate({
+    super.key,
+    required this.onLogin,
+    required this.onRegister,
+    required this.loginUsernameController,
+    required this.loginPasswordController,
+    required this.registerUsernameController,
+    required this.registerEmailController,
+    required this.registerPasswordController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Header(
+            title: 'Bienvenido',
+            subtitle: 'Por favor inicia sesión o regístrate para continuar',
+          ),
+          const SizedBox(height: 24.0),
+          LoginRegisterTabs(
+            onLogin: onLogin,
+            onRegister: onRegister,
+            loginUsernameController: loginUsernameController,
+            loginPasswordController: loginPasswordController,
+            registerUsernameController: registerUsernameController,
+            registerEmailController: registerEmailController,
+            registerPasswordController: registerPasswordController,
+          ),
+        ],
+      ),
+    );
+  }
+}
